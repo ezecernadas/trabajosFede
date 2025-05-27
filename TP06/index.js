f606003b9fbf934b37141b610e7e1e0c4424a84b
ezecernadas <ezecernadas@gmail.com>
2025-05-27 17:04:10 -0300

    Refactor index.html and index.js to improve layout and functionality for Pokémon display

--- TP06/index.js
+++ TP06/index.js
@@ -1,4 +1,94 @@
 /**
+ * Muestra una tarjeta de un Pokémon en el contenedor especificado.
+ *
+ * Si los datos del Pokémon son nulos, muestra un mensaje de error.
+ * De lo contrario, crea una tarjeta con la imagen del Pokémon y
+ * su nombre.
+ *
+ * @param {Object | null} pokemonData - Datos del Pokémon a mostrar.
+ * @param {string} containerId - ID del contenedor donde spawnear la tarjeta.
+ */
+function mostrarTarjetaPokemon(pokemonData, containerId) {
+    const container = document.getElementById(containerId);
+
+    // Si hubo error
+    if (!pokemonData) {
+        return;
+    }
+
+    // Crear tarjeta con datos válidos
+    const pokemonCard = document.createElement("div");
+    pokemonCard.classList.add("card");
+
+    // Imagen del Pokémon
+    const pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemonData.sprites.front_default;
+
+    // Contenedor de datos
+    const dataContainer = document.createElement("div");
+    dataContainer.classList.add("data");
+
+    // Nombre del Pokémon
+    const pokemonName = document.createElement("legend");
+    pokemonName.textContent = "Nombre: " + pokemonData.name;
+
+    // Añadir elementos a la tarjeta
+    pokemonCard.appendChild(pokemonImage);
+    pokemonCard.appendChild(dataContainer);
+    dataContainer.appendChild(pokemonName);
+
+    // Añadir tarjeta al contenedor
+    container.appendChild(pokemonCard);
+}
+
+/**
+ * Muestra un mensaje de error en el contenedor especificado.
+ *
+ * @param {string} containerId - ID del contenedor donde spawnear el mensaje de error.
+ */
+function mostrarTarjetaPokemonError(containerId) {
+    const container = document.getElementById(containerId);
+
+    // Crear tarjeta de error
+    const errorCard = document.createElement("div");
+    errorCard.classList.add("card");
+    errorCard.style.alignItems = "center";
+    errorCard.style.justifyContent = "center";
+
+    // Mensaje de error
+    const errorMessage = document.createElement("legend");
+    errorMessage.textContent = "No se encontro el Pokémon, revise el nombre escrito.";
+    errorCard.appendChild(errorMessage);
+    container.appendChild(errorCard);
+}
+
+/**
+ * Busca un pokemon por nombre y muestra su tarjeta y su linea de evolucion.
+ * @param {string} nombre nombre del pokemon a buscar
+ * @param {string} idSpawn id del div donde se va a spawnear la tarjeta
+ */
+async function buscarYMostrarLineaEvolutiva(nombre, idSpawn) {
+    const pokemon = await obtenerDatosPokemon(nombre);
+
+    if (!pokemon) {
+        mostrarTarjetaPokemonError(idSpawn);
+        return;
+    }
+
+    const evoluciones = await obtenerLineaEvolutiva(pokemon.id);
+
+    if (!evoluciones) {
+        console.error("No se encontraron evoluciones para el pokemon", nombre);
+        return;
+    }
+
+    evoluciones.forEach(async nombreEvolucion => {
+        const evolucion = await obtenerDatosPokemon(nombreEvolucion);
+        mostrarTarjetaPokemon(evolucion, idSpawn);
+    });
+}
+
+/**
  * Obtiene los datos de un Pokémon por su nombre.
  *
  * @param {string} nombre - El nombre del Pokémon.
@@ -16,13 +106,6 @@ async function obtenerDatosPokemon(nombre) {
         return null;
     }
 }
-
-// Of course, you are handling two different IDs there.
-
-//     Fetch the pokemon-species: https://pokeapi.co/api/v2/pokemon-species/ivysaur
-//     look at .evolution_chain -> https://pokeapi.co/api/v2/evolution-chain/1/
-//     Fetch the chain: https://pokeapi.co/api/v2/evolution-chain/1/
-//     Parse the JSON and get the full list of evolutions.
 
 /**
  * Obtiene la línea evolutiva de un Pokémon dado su ID.
@@ -70,104 +153,10 @@ async function obtenerLineaEvolutiva(idPoke) {
     }
 }
 
-/**
- * Muestra una tarjeta de un Pokémon en el contenedor especificado.
- *
- * Si los datos del Pokémon son nulos, muestra un mensaje de error.
- * De lo contrario, crea una tarjeta con la imagen del Pokémon y
- * su nombre.
- *
- * @param {Object | null} pokemonData - Datos del Pokémon a mostrar.
- * @param {string} containerId - ID del contenedor donde spawnear la tarjeta.
- */
-function mostrarTarjetaPokemon(pokemonData, containerId) {
-    const container = document.getElementById(containerId);
-
-    // Si hubo error
-    if (!pokemonData) {
-        return;
-    }
-
-    // Crear tarjeta con datos válidos
-    const pokemonCard = document.createElement("div");
-    pokemonCard.classList.add("card");
-
-    // Imagen del Pokémon
-    const pokemonImage = document.createElement("img");
-    pokemonImage.src = pokemonData.sprites.front_default;
-
-    // Contenedor de datos
-    const dataContainer = document.createElement("div");
-    dataContainer.classList.add("data");
-
-    // Nombre del Pokémon
-    const pokemonName = document.createElement("legend");
-    pokemonName.textContent = "Nombre: " + pokemonData.name;
-
-    // Añadir elementos a la tarjeta
-    pokemonCard.appendChild(pokemonImage);
-    pokemonCard.appendChild(dataContainer);
-    dataContainer.appendChild(pokemonName);
-
-    // Añadir tarjeta al contenedor
-    container.appendChild(pokemonCard);
-}
-
-function mostrarTarjetaPokemonError(containerId) {
-    const container = document.getElementById(containerId);
-
-    // Crear tarjeta de error
-    const errorCard = document.createElement("div");
-    errorCard.classList.add("card");
-    errorCard.style.alignItems = "center";
-    errorCard.style.justifyContent = "center";
-
-    // Mensaje de error
-    const errorMessage = document.createElement("legend");
-    errorMessage.textContent = "No se encontro el Pokémon, revise el nombre escrito.";
-    errorCard.appendChild(errorMessage);
-    container.appendChild(errorCard);
-}
-
-async function buscarYMostrar(nombre, idSpawn) {
-    const datos = await obtenerDatosPokemon(nombre);
-    console.log(datos);
-    mostrarTarjetaPokemon(datos, idSpawn);
-}
-
-/**
- * Busca un pokemon por nombre y muestra su tarjeta y su linea de evolucion.
- * @param {string} nombre nombre del pokemon a buscar
- * @param {string} idSpawn id del div donde se va a spawnear la tarjeta
- */
-async function buscarYMostrarLineaEvolutiva(nombre, idSpawn) {
-    const pokemon = await obtenerDatosPokemon(nombre);
-
-    if (!pokemon) {
-        mostrarTarjetaPokemonError(idSpawn);
-        return;
-    }
-
-    const evoluciones = await obtenerLineaEvolutiva(pokemon.id);
-
-    if (!evoluciones) {
-        console.error("No se encontraron evoluciones para el pokemon", nombre);
-        return;
-    }
-
-    evoluciones.forEach(async nombreEvolucion => {
-        const evolucion = await obtenerDatosPokemon(nombreEvolucion);
-        mostrarTarjetaPokemon(evolucion, idSpawn);
-    });
-}
-
 document.getElementById('mostrar').addEventListener('click',()=>{
-    document.getElementById('cardContainerTp1').innerHTML = '';
+    document.getElementById('cardContainerPunto1').innerHTML = '';
 
     const nombre = document.getElementById('nombre').value;
     
-    buscarYMostrarLineaEvolutiva(nombre, "cardContainerTp1");
-    // obtenerDatosPokemon("pikachu").then((datos) => {
-    //     console.log(datos); // También te muestra el resultado
-    // });
+    buscarYMostrarLineaEvolutiva(nombre, "cardContainerPunto1");
 });
