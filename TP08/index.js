5979339d77c482c09881856639f26bb1c22916b3
ezecernadas <ezecernadas@gmail.com>
2025-05-29 14:01:26 -0300

    variables por GET y POST

--- TP08/index.js
+++ TP08/index.js
@@ -3,13 +3,16 @@ const mysql = require ('mysql2');
 const api = express();
 
 require('dotenv').config();
+api.use(express.json);
 
 api.use(express.json());
 const db = mysql.createConnection({
     host :process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+    connectTimeout : 10000,
+    keepAliveInitialDelay: 10000,
 });
 
 db.connect((error)=>{
@@ -89,6 +92,52 @@ api.get('/areasDeRegion',(request, result)=>{
 
         result.json(resultado);
     })
+});
+
+api.get('/paises/:nombrePais',(request, result)=>{
+    const {nombrePais} = request.params;
+    db.query("SELECT * FROM `countries` WHERE `name` = ?", [nombrePais], (err,resultado)=>{
+        if(err){
+            result.status(500).json({message:err.message});
+        }
+
+        if(resultado.length === 0){
+            return result.status(404).json({message:"No se encontró el país"});
+        }
+
+        result.json(resultado[0]);
+    })
+});
+
+api.get('/caso1/:area/:region',(request, result)=>{
+    const {area, region} = request.params;
+    db.query("SELECT * FROM `countries` WHERE `area` > ? AND `region_id` = ?", [area, region], (err,resultado)=>{
+        if(err){
+            result.status(500).json({message:err.message});
+            return;
+        }
+
+        if(resultado == ''){
+            return result.status(404).json({message:"No se encontraron países que cumplan con los criterios"});
+        }
+
+        result.json(resultado);
+    });
+});
+
+api.post('/caso2',(request, result)=>{
+    const {area, region} = request.body;
+    db.query("SELECT * FROM `countries` WHERE `area` > ? AND `region_id` = ?", [area, region], (err,resultado)=>{
+        if(err){
+            result.status(500).json({message:err.message});
+        }
+
+        if(resultado == ''){
+            return result.status(404).json({message:"No se encontraron países que cumplan con los criterios"});
+        }
+
+        result.json(resultado);
+    });
 });
 
 const PORT = 3000;
